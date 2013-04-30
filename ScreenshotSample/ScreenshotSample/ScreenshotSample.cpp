b998b494de54b920e1a332e453518778ae688b5a
Phil Napieralski <phil@pnapieralski.com>
2013-04-29 23:34:11 -0700

    Sample complete. Adds "screenshot on click" functionality to stock spinning cube sample from Windows 8. Saves to %localappdata%/<Your app>/TempState folder

---
+++ ScreenshotSample/ScreenshotSample/ScreenshotSample.cpp
@@ -0,0 +1,149 @@
+﻿#include "pch.h"
+#include "ScreenshotSample.h"
+#include "BasicTimer.h"
+
+using namespace Windows::ApplicationModel;
+using namespace Windows::ApplicationModel::Core;
+using namespace Windows::ApplicationModel::Activation;
+using namespace Windows::UI::Core;
+using namespace Windows::System;
+using namespace Windows::Foundation;
+using namespace Windows::Graphics::Display;
+using namespace concurrency;
+
+ScreenshotSample::ScreenshotSample() :
+	m_windowClosed(false),
+	m_windowVisible(true)
+{
+}
+
+void ScreenshotSample::Initialize(CoreApplicationView^ applicationView)
+{
+	applicationView->Activated +=
+        ref new TypedEventHandler<CoreApplicationView^, IActivatedEventArgs^>(this, &ScreenshotSample::OnActivated);
+
+	CoreApplication::Suspending +=
+        ref new EventHandler<SuspendingEventArgs^>(this, &ScreenshotSample::OnSuspending);
+
+	CoreApplication::Resuming +=
+        ref new EventHandler<Platform::Object^>(this, &ScreenshotSample::OnResuming);
+
+	m_renderer = ref new CubeRenderer();
+}
+
+void ScreenshotSample::SetWindow(CoreWindow^ window)
+{
+	window->SizeChanged += 
+        ref new TypedEventHandler<CoreWindow^, WindowSizeChangedEventArgs^>(this, &ScreenshotSample::OnWindowSizeChanged);
+
+	window->VisibilityChanged +=
+		ref new TypedEventHandler<CoreWindow^, VisibilityChangedEventArgs^>(this, &ScreenshotSample::OnVisibilityChanged);
+
+	window->Closed += 
+        ref new TypedEventHandler<CoreWindow^, CoreWindowEventArgs^>(this, &ScreenshotSample::OnWindowClosed);
+
+	window->PointerCursor = ref new CoreCursor(CoreCursorType::Arrow, 0);
+
+	window->PointerPressed +=
+		ref new TypedEventHandler<CoreWindow^, PointerEventArgs^>(this, &ScreenshotSample::OnPointerPressed);
+
+	window->PointerMoved +=
+		ref new TypedEventHandler<CoreWindow^, PointerEventArgs^>(this, &ScreenshotSample::OnPointerMoved);
+
+	m_renderer->Initialize(CoreWindow::GetForCurrentThread());
+}
+
+void ScreenshotSample::Load(Platform::String^ entryPoint)
+{
+}
+
+void ScreenshotSample::Run()
+{
+	BasicTimer^ timer = ref new BasicTimer();
+
+	while (!m_windowClosed)
+	{
+		if (m_windowVisible)
+		{
+			timer->Update();
+			CoreWindow::GetForCurrentThread()->Dispatcher->ProcessEvents(CoreProcessEventsOption::ProcessAllIfPresent);
+			m_renderer->Update(timer->Total, timer->Delta);
+			m_renderer->Render();
+			m_renderer->Present(); // This call is synchronized to the display frame rate.
+		}
+		else
+		{
+			CoreWindow::GetForCurrentThread()->Dispatcher->ProcessEvents(CoreProcessEventsOption::ProcessOneAndAllPending);
+		}
+	}
+}
+
+void ScreenshotSample::Uninitialize()
+{
+}
+
+void ScreenshotSample::OnWindowSizeChanged(CoreWindow^ sender, WindowSizeChangedEventArgs^ args)
+{
+	m_renderer->UpdateForWindowSizeChange();
+}
+
+void ScreenshotSample::OnVisibilityChanged(CoreWindow^ sender, VisibilityChangedEventArgs^ args)
+{
+	m_windowVisible = args->Visible;
+}
+
+void ScreenshotSample::OnWindowClosed(CoreWindow^ sender, CoreWindowEventArgs^ args)
+{
+	m_windowClosed = true;
+}
+
+void ScreenshotSample::OnPointerPressed(CoreWindow^ sender, PointerEventArgs^ args)
+{
+	m_renderer->TakeScreenshot();
+}
+
+void ScreenshotSample::OnPointerMoved(CoreWindow^ sender, PointerEventArgs^ args)
+{
+	// Insert your code here.
+}
+
+void ScreenshotSample::OnActivated(CoreApplicationView^ applicationView, IActivatedEventArgs^ args)
+{
+	CoreWindow::GetForCurrentThread()->Activate();
+}
+
+void ScreenshotSample::OnSuspending(Platform::Object^ sender, SuspendingEventArgs^ args)
+{
+	// Save app state asynchronously after requesting a deferral. Holding a deferral
+	// indicates that the application is busy performing suspending operations. Be
+	// aware that a deferral may not be held indefinitely. After about five seconds,
+	// the app will be forced to exit.
+	SuspendingDeferral^ deferral = args->SuspendingOperation->GetDeferral();
+
+	create_task([this, deferral]()
+	{
+		// Insert your code here.
+
+		deferral->Complete();
+	});
+}
+ 
+void ScreenshotSample::OnResuming(Platform::Object^ sender, Platform::Object^ args)
+{
+	// Restore any data or state that was unloaded on suspend. By default, data
+	// and state are persisted when resuming from suspend. Note that this event
+	// does not occur if the app was previously terminated.
+}
+
+IFrameworkView^ Direct3DApplicationSource::CreateView()
+{
+    return ref new ScreenshotSample();
+}
+
+[Platform::MTAThread]
+int main(Platform::Array<Platform::String^>^)
+{
+	auto direct3DApplicationSource = ref new Direct3DApplicationSource();
+	CoreApplication::Run(direct3DApplicationSource);
+	return 0;
+}
